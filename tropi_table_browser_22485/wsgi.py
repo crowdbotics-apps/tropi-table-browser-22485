9909840cd20d2e6cfd034d0f8cc25bacd9299db5
Crowdbotics <team@crowdbotics.com>
2020-11-10 18:50:09 +0000

    Initial commit.

---
+++ tropi_table_browser_22485/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tropi_table_browser_22485 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tropi_table_browser_22485.settings')
+
+application = get_wsgi_application()
